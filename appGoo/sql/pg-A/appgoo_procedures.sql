cba86fb39760a6abbab05e167aac695343d48e1f
mrpanda <stephen.alleyn@gmail.com>
2019-01-21 08:28:13 +1100

    fixed dir structure

---
+++ appGoo/sql/pg-A/appgoo_procedures.sql
@@ -0,0 +1,10 @@
+# 
+# appgoo_procedures.sql
+#
+# instals the prcedures in the correct order
+#
+<%include /procs/helpers/cast_functions.sql %>
+<%include /procs/helpers/create_sequence.sql %>
+<%include /procs/helpers/is_numeric.sql %>
+<%include /procs/helpers/permission_functions.sql %>
+<%include /procs/helpers/url_functions.sql %>
